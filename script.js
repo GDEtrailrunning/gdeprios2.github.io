ff6ba9f143f33858eaf2bc6b22e2e1156977494c
GDEtrailrunning <gdeconcienciacorporal@gmail.com>
2026-03-25 13:05:11 -0300

    Subo semana 94

--- script.js
+++ script.js
@@ -1,37 +1,33 @@
 document.addEventListener("DOMContentLoaded", function () {
   actualizarDiasRestantes();
-  setInterval(actualizarDiasRestantes, 24*60*60*1000); // Actualizar diario
+  setInterval(actualizarDiasRestantes, 24*60*60*1000);
   
-  // Crear dos nuevas instancias de HTMLVideoElement para cada sección
-  let Semana_1_video_1 = document.createElement("video");
-  let Semana_1_video_2 = document.createElement("video");
-  let Semana_2_video_3 = document.createElement("video");
-  let Semana_2_video_4 = document.createElement("video");
+  let video1 = document.createElement("video");
+  let video2 = document.createElement("video");
+  let video3 = document.createElement("video");
+  let video4 = document.createElement("video");
 
-  // Establecer las fuentes de video
-  Semana_1_video_1.src = "video 1.mp4"; // ruta de su primer video
-  Semana_1_video_2.src = "video 2.mp4"; // ruta de su segundo video
-  Semana_2_video_3.src = "video 3.mp4"; // ruta de su tercer video
-  Semana_2_video_4.src = "video 4.mp4"; // ruta de su cuarto video
+  video1.src = "video 1.mp4";
+  video2.src = "video 2.mp4";
+  video3.src = "video 3.mp4";
+  video4.src = "video 4.mp4";
 
-  // Establecer atributos de video
-  Semana_1_video_1.controls = true;
-  Semana_1_video_1.autoplay = false;
-  Semana_1_video_1.loop = false;
+  video1.controls = true;
+  video1.autoplay = false;
+  video1.loop = false;
 
-  Semana_1_video_2.controls = true;
-  Semana_1_video_2.autoplay = false;
-  Semana_1_video_2.loop = false;
+  video2.controls = true;
+  video2.autoplay = false;
+  video2.loop = false;
 
-  Semana_2_video_3.controls = true;
-  Semana_2_video_3.autoplay = false;
-  Semana_2_video_3.loop = false;
+  video3.controls = true;
+  video3.autoplay = false;
+  video3.loop = false;
 
-  Semana_2_video_4.controls = true;
-  Semana_2_video_4.autoplay = false;
-  Semana_2_video_4.loop = false;
+  video4.controls = true;
+  video4.autoplay = false;
+  video4.loop = false;
 
-  // Agregar los dos videos a cada sección
   document.getElementById("Semana 1").appendChild(video1);
   document.getElementById("Semana 1").appendChild(video2);
   document.getElementById("Semana 2").appendChild(video3);
